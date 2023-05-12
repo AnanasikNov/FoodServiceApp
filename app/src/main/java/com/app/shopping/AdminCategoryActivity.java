5bc5029299da33418b5319fd2ee47cd2bbb15e95
AnanasikNov <novinkinaa.i20@gmail.com>
2023-05-12 14:49:10 +0300

    main

---
+++ app/src/main/java/com/app/shopping/AdminCategoryActivity.java
@@ -0,0 +1,89 @@
+package com.app.shopping;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminCategoryActivity extends AppCompatActivity {
+    private ImageView shaurma, pizza, burgers, potatoesfried;
+    private Button LogoutBtn, CheckOrdersBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+        LogoutBtn = (Button) findViewById(R.id.admin_logout_btn);
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(AdminCategoryActivity.this,MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+        CheckOrdersBtn = (Button) findViewById(R.id.check_orders_btn);
+
+
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(AdminCategoryActivity.this,AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        shaurma = (ImageView) findViewById(R.id.earrings);
+        pizza = (ImageView) findViewById(R.id.pendants);
+        burgers = (ImageView) findViewById(R.id.wristband_wallets);
+        potatoesfried = (ImageView) findViewById(R.id.watches);
+
+
+        shaurma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, com.app.shopping.AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Шаурма");
+                startActivity(intent);
+            }
+        });
+
+
+        pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, com.app.shopping.AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Пицца");
+                startActivity(intent);
+            }
+        });
+
+        burgers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, com.app.shopping.AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Бургеры");
+                startActivity(intent);
+            }
+        });
+
+        potatoesfried.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, com.app.shopping.AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Картофель фри");
+                startActivity(intent);
+            }
+        });
+    }
+}
